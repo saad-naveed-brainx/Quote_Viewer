4d3c06150395c89e9926414a6e156b4b417029a3
BrainX Flutter <brainxflutter@Macs-MacBook-Pro.local>
2025-04-30 17:54:32 +0500

    emty dart file

---
+++ lib/widgets/detailView/detail_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DetailView extends StatelessWidget {
+  const DetailView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Detail View')));
+  }
+}
